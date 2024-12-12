725da84a80c7c87d333d7aab076be6f9d79977c3
Gitmarkkkk <mlp23061@gmail.com>
2024-12-12 09:48:43 -0600

    Update FInalFolder.java

--- FinalProject144/src/finalproject144/FInalFolder.java
+++ FinalProject144/src/finalproject144/FInalFolder.java
@@ -1,16 +1,28 @@
 
 package finalproject144;
 
-import com.sun.jdi.connect.spi.Connection;
 
+import java.sql.*;
 
-public class FInalFolder {
+public class FinalFolder {
 Connection con;
-public FInalFolder(){
-    String url="jdc.mysql://localhost:3306/finalproject";
+PreparedStatement myquery;
+public FinalFolder(){
+    String url="jdc.mysql://localhost:3306/FinalProject";
     String login="root";
     String password="";
+    System.out.print("Congrats!");
+    try{
+        con=DriverManager.getConnection(url, login, password);
+    }
+    catch(SQLException ex){
+        ex.printStackTrace();
+    }
     
+}
+public static void InsertMethod(String l, String f, String ft, String n, String a1, String a2, String c, String s){
+    String SqlCode="insert into employees"+"(lastname, firstnme, phoneType, phoneNumber, address1, address2, city, state)"
+            + ""+"values ("+l+","+f+","+ft+","+n+","+a1+","+a2+","+c+","+s+");";
     
 }
 }
